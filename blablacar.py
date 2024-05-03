c93ffac4e6a5b0f546d35bd9daee590c87b0a2d2
Dima <dimazzz1975@mail.ru>
2024-05-04 00:53:26 +0300

    Сумма из 3

--- blablacar.py
+++ blablacar.py
@@ -1,7 +1,9 @@
 def calculate_sum():
     a = float(input("Введите первое число: "))
     b = float(input("Введите второе число: "))
-    c = a + b
-    print(f'Сумма равна: {c}')
-
+    c = float(input("Введите третье число: "))
+    sum_ab = a + b
+    sum_abc = sum_ab + c
+    print(f'Сумма всех трех чисел равна: {sum_abc}')
+    
 calculate_sum()
